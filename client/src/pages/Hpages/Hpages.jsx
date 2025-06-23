8bc55d7ceff50c886045b05493f155e522d8a112
2115425Amar <amar2115425@akgec.ac.in>
2025-06-23 22:31:58 +0530

    ui updated

--- client/src/pages/Hpages/Hpages.jsx
+++ client/src/pages/Hpages/Hpages.jsx
@@ -1,42 +1,46 @@
-import React, { useContext } from 'react'
+import { useContext } from "react";
+import SearchBar from "../../components/searchBar/SearchBar";
 import "./Hpages.scss"
-import SearchBar from '../../components/searchBar/SearchBar'
-import { AuthContext } from '../../context/AuthContext';
+import { AuthContext } from "../../context/AuthContext";
 
-function Hpages() {
+function HomePage() {
 
-  const {currentUser} = useContext(AuthContext);
-  
+  const {currentUser} = useContext(AuthContext)
   console.log(currentUser);
-  
+
   return (
-    <div className='Hpages'>
+    <div className="homePage">
       <div className="textContainer">
         <div className="wrapper">
-            <h1>Find Real Estate & Get Your Dream Place</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, consequatur eligendi soluta in assumenda natus dolorum labore officia architecto minima distinctio qui velit animi iusto itaque consectetur! Tempore, dolorem ex.</p>
-            <SearchBar/>
-            <div className="boxes">
-                <div className="box">
-                    <h1>16+</h1>
-                    <h2>Year of Experience</h2>
-                </div>
-                <div className="box">
-                    <h1>200</h1>
-                    <h2>Award Gained</h2>
-                </div>
-                <div className="box">
-                    <h1>1200+</h1>
-                    <h2>Property Ready</h2>
-                </div>
+          <h1 className="title">Find Real Estate & Get Your Dream Place</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos
+            explicabo suscipit cum eius, iure est nulla animi consequatur
+            facilis id pariatur fugit quos laudantium temporibus dolor ea
+            repellat provident impedit!
+          </p>
+          <SearchBar />
+          <div className="boxes">
+            <div className="box">
+              <h1>16+</h1>
+              <h2>Years of Experience</h2>
             </div>
+            <div className="box">
+              <h1>200</h1>
+              <h2>Award Gained</h2>
+            </div>
+            <div className="box">
+              <h1>2000+</h1>
+              <h2>Property Ready</h2>
+            </div>
+          </div>
         </div>
       </div>
       <div className="imgContainer">
-        <img src="bg4.png" alt="" />
+        <img src="/bg4.png" alt="" />
       </div>
     </div>
-  )
+  );
 }
 
-export default Hpages
+export default HomePage;
