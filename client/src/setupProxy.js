c0e47b79375f6599cc56b2035e584a0423ee5089
2115425Amar <amar2115425@akgec.ac.in>
2025-07-18 02:08:35 +0530

    Add VITE_API_URL for production

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,11 +1,15 @@
+// setupProxy.js or any Node script
+require('dotenv').config(); // add this at the top
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
-  app.use(
-    '/api',
-    createProxyMiddleware({
-    target: `${import.meta.env.VITE_API_URL}/api`,
-    changeOrigin: true,
-    })
-  );
+  if (process.env.NODE_ENV === 'development') {
+    app.use(
+      '/api',
+      createProxyMiddleware({
+        target: `${process.env.VITE_API_URL}/api`,
+        changeOrigin: true,
+      })
+    );
+  }
 };
