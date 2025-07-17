fa118269e9adf41aafe8558a9b82ff26a052f9d2
2115425Amar <amar2115425@akgec.ac.in>
2025-07-18 02:29:02 +0530

    Deploy socket server

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,15 +1,11 @@
-// setupProxy.js or any Node script
-require('dotenv').config(); // add this at the top
-const { createProxyMiddleware } = require('http-proxy-middleware');
+// const { createProxyMiddleware } = require('http-proxy-middleware');
 
-module.exports = function(app) {
-  if (process.env.NODE_ENV === 'development') {
-    app.use(
-      '/api',
-      createProxyMiddleware({
-        target: `${process.env.VITE_API_URL}/api`,
-        changeOrigin: true,
-      })
-    );
-  }
-};
+// module.exports = function(app) {
+//   app.use(
+//     '/api',
+//     createProxyMiddleware({
+//     target: `${import.meta.env.VITE_API_URL}/api`,
+//     changeOrigin: true,
+//     })
+//   );
+// };
