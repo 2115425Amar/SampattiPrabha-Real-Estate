d92dc27804f34a4df20d58721361d1b6b37a51b9
2115425Amar <amar2115425@akgec.ac.in>
2025-07-18 10:48:52 +0530

    Fix: Removed duplicate listen() on io object

--- socket/app.js
+++ socket/app.js
@@ -1,21 +1,19 @@
 import express from "express";
-import {Server} from "socket.io"
+import { Server } from "socket.io";
 import http from "http";
 import cors from "cors";
 import dotenv from "dotenv";
-dotenv.config(); 
+dotenv.config();
 
 const app = express();
 const server = http.createServer(app);
 
-// CORS setup
 app.use(cors({
   origin: [process.env.CLIENT_URL || "http://localhost:5173"],
   methods: ["GET", "POST"],
   credentials: true
 }));
 
-// Optional test route
 app.get("/", (req, res) => {
   res.send("Socket.io server is running!");
 });
@@ -28,7 +26,8 @@ const io = new Server(server, {
   }
 });
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 8080;
+
 server.listen(PORT, () => {
   console.log(`Socket.io server running on port ${PORT}`);
 });
@@ -36,44 +35,39 @@ server.listen(PORT, () => {
 let onlineUser = [];
 
 const addUser = (userId, socketId) => {
-    const userExits = onlineUser.find((user) => user.userId === userId);
-    if (!userExits) {
-      onlineUser.push({ userId, socketId });
-    }
-  };
-  
-  const removeUser = (socketId) => {
-    onlineUser = onlineUser.filter((user) => user.socketId !== socketId);
-  };
-  
-  const getUser = (userId) => {
-    return onlineUser.find((user) => user.userId === userId);
-  };
-  
-  io.on("connection", (socket) => {
-    console.log("New client connected:", socket.id);
+  const userExits = onlineUser.find((user) => user.userId === userId);
+  if (!userExits) {
+    onlineUser.push({ userId, socketId });
+  }
+};
 
-    socket.on("newUser", (userId) => {
-      addUser(userId, socket.id);
-      console.log("Online users:", onlineUser);
-    });
-  
-    socket.on("sendMessage", ({ receiverId, data }) => {
-      const receiver = getUser(receiverId);
-      if (receiver) {
-        io.to(receiver.socketId).emit("getMessage", data);
-      }
-       else {
-        console.log(`User with ID ${receiverId} is not connected.`);
-      }
-    });
-  
-    socket.on("disconnect", () => {
-      removeUser(socket.id);
-      console.log("User disconnected:", socket.id);
-    });
+const removeUser = (socketId) => {
+  onlineUser = onlineUser.filter((user) => user.socketId !== socketId);
+};
+
+const getUser = (userId) => {
+  return onlineUser.find((user) => user.userId === userId);
+};
+
+io.on("connection", (socket) => {
+  console.log("New client connected:", socket.id);
+
+  socket.on("newUser", (userId) => {
+    addUser(userId, socket.id);
+    console.log("Online users:", onlineUser);
   });
-  
-io.listen(PORT, () => {
-  console.log(`Socket.io server listening on port ${PORT}`);
+
+  socket.on("sendMessage", ({ receiverId, data }) => {
+    const receiver = getUser(receiverId);
+    if (receiver) {
+      io.to(receiver.socketId).emit("getMessage", data);
+    } else {
+      console.log(`User with ID ${receiverId} is not connected.`);
+    }
+  });
+
+  socket.on("disconnect", () => {
+    removeUser(socket.id);
+    console.log("User disconnected:", socket.id);
+  });
 });
